7be98ddcf4b01ec6d0832f5a163c732b38fd8ad7
aubustou <survivalfr@yahoo.fr>
2021-04-11 14:22:46 +0200

    Add setup.py and gitignore

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name="cwc_cartes",
+    version="1.0",
+    packages=["cwc_cartes"],
+    url="",
+    license="MIT",
+    author="Aubustou",
+    author_email="survivalfr@yahoo.fr",
+    description=(
+        "Create helper cards for Cold War Commander from list of units and " "pictures"
+    ),
+    python_requires=">=3.9",
+    install_requires=[
+        "setuptools==49.2.1",
+        "Pillow==8.2.0",
+        "apischema==0.14.7",
+    ],
+)
